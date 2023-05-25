8635b586f44261e67c907efd1b7a96dfc69019c3
Jeremy Ng <jngchenghin@gmail.com>
2023-05-25 01:48:51 -0700

    New color struct

---
+++ color.h
@@ -0,0 +1,8 @@
+#include "tgaimage.h"
+
+struct Color {
+    const TGAColor white = TGAColor(255, 255, 255, 255);
+    const TGAColor red = TGAColor(255, 0,   0,   255);
+    const TGAColor blue = TGAColor(0, 0,   255,   255);
+    const TGAColor green = TGAColor(0, 255,  0,   255);
+};
